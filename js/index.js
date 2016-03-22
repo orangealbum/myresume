a628b790fc6e1e406b349f50e51319f5c7dc7422
orangealbum <orangealbum@outlook.com>
2016-03-22 16:49:13 +0800

    第一个成品版本，完善了一些东西，然后加入了自己的简历

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
+  var menus= ['首页', '项目', '技能', '教育', '实习', '简历']
   $('#pagepiling').pagepiling({
-     loopBottom: true
+     loopBottom: true,
+     navigation:{
+       'tooltips':menus
+     }
   });
 })
